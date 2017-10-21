db9600f068c1975bdd4e2fb47532fdff19903e08
André Luiz Rodrigues Estevam <a166348@g.unicamp.br>
2017-10-21 15:49:44 -0200

    Update pydeijao.py

--- pydeijao.py
+++ pydeijao.py
@@ -2,13 +2,14 @@ import requests as reqs
 from bs4 import BeautifulSoup as bs, Comment
 import examplehtml
 
+# Use the local html?
 isExample = False
 
 class Pydeijao:
     def __init__(self):
         print('Starting Pydeijão...')
-        self.encoding = 'utf-8'
-        self.link = 'http://www.pfl.unicamp.br/Rest/view/site/cardapio.php'
+        self.encoding = 'utf-8' #website encoding
+        self.link = 'http://www.pfl.unicamp.br/Rest/view/site/cardapio.php' #base link
         self.soup = ''
         print('\tDone.\n')
 
@@ -19,15 +20,19 @@ class Pydeijao:
 
     def run(self):
         print('Accessing the page...')
-        self.soup = self.loadPage(self.link)
+        self.soup = self.loadPage(self.link) # Accessing
         print('\tDone.\n')
+		
+		#Removing comments
         comments = self.soup.findAll(text=lambda text: isinstance(text, Comment))
         for comment in comments:
             comment.extract()
 
+		#Getting all <tr></tr>
         self.soup = self.soup.find_all('tr')
 
-        if not self.verifyValid()[0]:
+		#Verifying if the menu is avaliable
+        if not self.verifyValid()[0]:#Not avaliable
             print('\n{:s}'.format(self.getDecorator(50)))
             print('| /!\ The menu could not be loaded.\n'
                   '| The page says: {}'.format(self.verifyValid()[1]), end='')
@@ -35,7 +40,7 @@ class Pydeijao:
             input()
             exit(0)
 
-        else:
+        else:#Avaliable
             self.printSpace()
 
             print(self.getDecorator(53) + '\n')
@@ -77,23 +82,27 @@ class Pydeijao:
 
 
     def extractDay(self):
+		"""Extracts the day information about the menu"""
         daystr = self.soup[0].getText()
         #daystr = daystr.strip()
         baseList = daystr.split(' - ')
         return baseList[1].strip().split(' ')
 
     def extractTitle(self):
+		"""Extrating a title for the menu"""
         tstr = self.soup[0].getText()
         tlst = tstr.split(' - ')
         return tlst[0].strip()
 
 
     def verifyValid(self):
+		"""Verifies if there is a menu to be loaded"""
         verifyStr = self.soup[4].getText().strip()
         return [not verifyStr == 'NÃO HÁ CARDÁPIO CADASTRADO!', verifyStr]
 
 
     def extractMenuLunch(self):
+		"""Extracts the menu itself"""
         lst = self.soup[3].getText().split('\n')
         newlst = []
 
@@ -102,27 +111,22 @@ class Pydeijao:
                 newlst.append(elem)
 
         rsp = []
-        rsp.append(['PEDREIRAGEM', newlst[0]])
+        rsp.append(['', newlst[0]])#Default
 
         for elem in newlst[1:7]:
             tupelem = elem.split(':')
-
             tupelem[0] = tupelem[0].strip()
             tupelem[1] = tupelem[1].strip()
-
             rsp.append(tupelem)
-
-        # newlst[7] = newlst[7].replace(':', '')
-        # newlst[8] = newlst[8].strip()
-        # rsp.append([newlst[7], newlst[8]])
-
         return rsp
 
     def extractType(self):
+		"""Extracts information about time"""
         lst = self.soup[2].getText().replace('\n\n\n\n', '').split()
         return lst
 
     def extractObservations(self):
+		"""Extracts observations"""
         lst = self.soup[3].getText().split('\n')
         newlst = []
 
@@ -135,9 +139,11 @@ class Pydeijao:
         return [newlst[7], newlst[8]]
 
     def getDecorator(self, size):
+		"""Makes a decorator string"""
         return '+' + ('-' * (size - 2)) + '+'
 
     def printSpace(self):
+		"""Print some white lines"""
         print('\n'*5)
 
 p = Pydeijao()
